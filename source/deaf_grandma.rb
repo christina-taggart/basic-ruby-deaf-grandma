1d2afc1e427665f0149db6a0202a22c7faf020a1
Apprentice <apprentice@dbc08.local>
2014-01-06 17:00:18 -0800

    Completed Release 2

--- source/deaf_grandma.rb
+++ source/deaf_grandma.rb
@@ -7,8 +7,31 @@
 
 # You'll probably want to write other methods, but this
 # encapsulates the core Grandma logic
-def deaf_grandma
+class Grandma
+
+  def initialize(name)
+    @name = name
+    @previous_input = nil
+  end
+
+  def deaf_grandma
+    puts "Say something to grandma:"
+    input = gets.chomp!
+    if input == "I love ya, Grandma, but I've got to go." || (input == "" && @previous_input == "")
+      puts "Grandma leaves the room."
+    elsif input != input.upcase
+      puts "HUH?! SPEAK UP, SONNY!"
+      @previous_input = input
+      deaf_grandma
+    else #yes shouting, but input is not specific phrase
+      puts "NO, NOT SINCE 1983!"
+      @previous_input = input
+      deaf_grandma
+    end
+  end
+
 end
 
 # Run our method
-deaf_grandma
+grandma = Grandma.new("grandma")
+grandma.deaf_grandma
