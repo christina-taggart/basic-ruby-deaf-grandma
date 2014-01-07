b53e8da9d0d1ded51133b5ea9d0df4cb3875d82b
Marion <knight499@gmail.com>
2014-01-06 16:54:35 -0800

    Adds original files

--- source/deaf_grandma.rb
+++ source/deaf_grandma.rb
@@ -8,6 +8,24 @@
 # You'll probably want to write other methods, but this
 # encapsulates the core Grandma logic
 def deaf_grandma
+  input = "input"
+  space_counter = 0
+  until input == "I love ya, Grandma, but I've got to go." || (input == "" && space_counter == 2)
+    input = gets.chomp
+    if input == "I love ya, Grandma, but I've got to go."
+      puts "BYE BYE!"
+    elsif input.upcase != input
+      puts "HUH?! SPEAK UP, SONNY!"
+    elsif input.upcase == input && input != ""
+      puts "NO, NOT SINCE 1983!"
+    end
+
+    if input != ""
+      space_counter = 0
+    elsif input == ""
+      space_counter += 1
+    end
+  end
 end
 
 # Run our method
