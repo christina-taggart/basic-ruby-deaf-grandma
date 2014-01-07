26a10707ea81434eaadd11b0444a3d56888d5baa
Apprentice <apprentice@dbc29.local>
2014-01-06 17:37:54 -0800

    release 2

--- source/deaf_grandma.rb
+++ source/deaf_grandma.rb
@@ -8,10 +8,11 @@
 # You'll probably want to write other methods, but this
 # encapsulates the core Grandma logic
 def deaf_grandma
-  user_input = gets.chomp
-  while user_input != "I love ya, Grandma, but I've got to go."
-    puts user_input != user_input.upcase ? "HUH?! SPEAK UP, SONNY!" : "NO, NOT SINCE 1983!"
-    user_input = gets.chomp
+  user_input = []
+  user_input << gets.chomp
+  while user_input[-1] != "I love ya, Grandma, but I've got to go." && !(user_input[-1] == "" && user_input[-2] == "")
+    puts user_input[-1] != user_input[-1].upcase ? "HUH?! SPEAK UP, SONNY!" : "NO, NOT SINCE 1983!"
+    user_input << gets.chomp
   end
 end
 
